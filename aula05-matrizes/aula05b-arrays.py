b8787cc4c83d6503d693e28557291abecd30fd7d
leonardo-scotti <leonardo.scotti07@gmail.com>
2026-05-04 20:31:20 -0300

    Aula 05

---
+++ aula05-matrizes/aula05b-arrays.py
@@ -0,0 +1,16 @@
+lista_frutas = ["Banana", "Maça", "Melancia"]
+
+print(lista_frutas)
+
+lista_frutas.append("Laranja")
+
+print(lista_frutas[3])
+print()
+
+for i in range(len(lista_frutas)):
+    print(i)
+
+print()
+
+for fruta in lista_frutas:
+    print(fruta)
